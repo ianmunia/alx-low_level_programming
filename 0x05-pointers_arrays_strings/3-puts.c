eff1b8876590d0bed9fafc8be38874d00950adc0
ianmunia <ianmunia@gmail.com>
2023-06-26 18:09:55 -0700

    input string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a sring
+ * *@str: the string to print
+ * Return: void
+ */
+void _puts(char *str)
+{
+	for (; *str != '0'; str++)
+	{
+	_putchar(*str);
+	}
+	_putchar9('\n');
+}
